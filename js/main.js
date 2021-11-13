f036b3fd295194702b112dcac86c866aa0ae3766
pericles001 <periclesadjovi@outlook.fr>
2021-11-13 19:15:57 +0100

    Update code base

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
+renderer.setClearColor(0xb7c3f3, 1);
 
-const light = new THREE.AmbientLight( 0xffffff, 0.5 ); // soft white light
-scene.add( light );
+const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
+scene.add(light);
 
 /*
 const geometry = new THREE.BoxGeometry();
@@ -19,23 +20,39 @@ scene.add( cube );
 camera.position.z = 5;
 
 const loader = new THREE.GLTFLoader();
-loader.load("../models/scene.gltf", function(gltf){
-    scene.add(gltf.scene);
-    gltf.scene.scale.set(.4, .4, .4);
-});
+
+
+class Doll {
+    constructor(model, position, rotation) {
+        loader.load("../models/scene.gltf", (gltf) => {
+            scene.add(gltf.scene);
+            gltf.scene.scale.set(.4, .4, .4);
+            gltf.scene.position.set(0, -1, 0);
+            this.doll = gltf.scene;
+        })
+    }
+    lookBack() {
+        this.doll.rotation.y = 1;
+    }
+}
+
+let doll = new Doll();
+setTimeout(() => {
+    doll.lookBack();
+}, 1000);
 
 function animate() {
-	requestAnimationFrame( animate );
-    cube.rotation.z += 0.01;
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+   
 }
 animate();
 
 window.addEventListener('resize', onWindowResize, false);
 
-function onWindowResize(){
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    render.setSize( window.innerWidth, window.innerHeight)
+    render.setSize(window.innerWidth, window.innerHeight);
 }
