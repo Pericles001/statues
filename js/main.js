e5abe60947aeb7f0dbb5eaa0c668c70d786fd547
pericles001 <periclesadjovi@outlook.fr>
2021-11-13 23:53:04 +0100

    Done with init

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ scene.add(light);
 //globnal variables
 const start_position = 3
 const end_position = -start_position
+const text = document.querySelector('.text')
+const TIMIT_LIMIT = 10
 /*
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -21,7 +23,7 @@ scene.add( cube );
 */
 
 function createCube(size, positionX, rotY = 0, color = 0xfbc851) {
-    const geometry = new THREE.BoxGeometry(size.w,  size.h, size.d);
+    const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
     const material = new THREE.MeshBasicMaterial({ color: color });
     const cube = new THREE.Mesh(geometry, material);
     cube.position.x = positionX;
@@ -34,8 +36,8 @@ camera.position.z = 5;
 
 const loader = new THREE.GLTFLoader()
 
-function delay(ms){
-    return new Promise( resolve => setTimeout(resolve, ms) );
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 class Doll {
@@ -56,31 +58,31 @@ class Doll {
         gsap.to(this.doll.rotation, { y: 0, duration: .45 })
     }
 
-    start(){
+    async start() {
         this.lookBackward()
-        await delay(1000)
+        await delay((Math.random() * 1000) + 1000)
         this.lookForward()
-        await delay(1000)
+        await delay((Math.random() * 750) + 750)
         this.start()
     }
 }
 
 function createTrack() {
-    createCube({w: start_position * 2 + .2, h: 1.5, d: 1},0, 0, 0xe5a716).position.z = -1;
-    createCube({w: .2, h: 1.5, d: 1}, start_position, -.35);
-    createCube({w: .2, h: 1.5, d: 1}, end_position, -.35);
- 
+    createCube({ w: start_position * 2 + .2, h: 1.5, d: 1 }, 0, 0, 0xe5a716).position.z = -1;
+    createCube({ w: .2, h: 1.5, d: 1 }, start_position, -.35);
+    createCube({ w: .2, h: 1.5, d: 1 }, end_position, -.35);
+
 }
 createTrack();
 
-class Player{
-    constructor(){
-        const geometry = new THREE.SphereGeometry( .3, 32, 16 );
-        const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-        const sphere = new THREE.Mesh( geometry, material );
+class Player {
+    constructor() {
+        const geometry = new THREE.SphereGeometry(.3, 32, 16);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        const sphere = new THREE.Mesh(geometry, material);
         sphere.position.z = 1
         sphere.position.x = start_position
-        scene.add( sphere );
+        scene.add(sphere);
         this.player = sphere
         this.playerInfo = {
             positionX: start_position,
@@ -89,16 +91,16 @@ class Player{
         }
     }
 
-    run(){
-this.playerInfo.velocity = .03
+    run() {
+        this.playerInfo.velocity = .03
     }
 
-    stop(){
+    stop() {
         //his.playerInfo.velocity = 0
-        gsap.to(this.playerInfo, {velocity: 0, duration: .1})
+        gsap.to(this.playerInfo, { velocity: 0, duration: .1 })
     }
 
-    update(){
+    update() {
         this.playerInfo.positionX -= this.playerInfo.velocity
         this.player.position.x = this.playerInfo.positionX
     }
@@ -108,6 +110,28 @@ const player = new Player()
 
 let doll = new Doll()
 
+async function init() {
+    await delay(500)
+    text.innerText = "Starting in 3"
+    await delay(500)
+    text.innerText = "Starting in 2"
+    await delay(500)
+    text.innerText = "Starting in 1"
+    await delay(500)
+    text.innerText = "Goo !!!"
+    startGame()
+}
+
+function startGame() {
+    let progressBar = createCube({ w: 5, h: .1, d: 1 }, 0)
+    progressBar.position.y = 3.35
+    gsap.to(progressBar.scale, {x: 0, duration: TIMIT_LIMIT, ease: "none"})
+    doll.start()
+
+}
+
+init()
+
 setTimeout(() => {
     doll.lookBackward()
 }, 1000);
@@ -116,7 +140,7 @@ function animate() {
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
     player.update()
- 
+
 }
 animate();
 
@@ -130,12 +154,12 @@ function onWindowResize() {
 }
 
 window.addEventListener('keydown', (e) => {
-    if(e.key == "ArrowUp"){
+    if (e.key == "ArrowUp") {
         player.run()
     }
 })
 window.addEventListener('keyup', (e) => {
-    if(e.key == "ArrowUp"){
+    if (e.key == "ArrowUp") {
         player.stop()
     }
 })
